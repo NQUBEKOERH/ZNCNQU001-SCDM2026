10422f698e66b9f51cd33b8b3027910b350402f3
NQUBEKOERH <132855378+NQUBEKOERH@users.noreply.github.com>
2026-03-11 21:18:46 +0200

    Add files via upload

---
+++ P2/ZNCNQU001_CTD.py
@@ -0,0 +1,13 @@
+# -*- coding: utf-8 -*-
+"""
+Created on Tue Mar 10 13:50:11 2026
+
+@author: zncnqu001
+"""
+
+#importing pandas as pd
+import os
+import pandas as pd
+#load the data file
+os.chdir("C:/Users/zncnqu001/Downloads/P1")
+df = pd.read_csv("CTD_TEMP_SALINITY_PROFILE.dat", sep = "\t")
